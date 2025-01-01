6a02d59553fd7fc844a1552617ea2b593a3c44f8
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2025-01-01 17:14:56 +0000

    Moneymaking problem from codechef

---
+++ CodeChef/Moneymaking.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin >> x >> y;
+	int nugget_sp = 5000*x;
+	int star_piece = 9800*y;
+	int total = nugget_sp + star_piece;
+	cout << total;
+}
