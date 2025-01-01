7ee89903f6d23c42a334b9750ce4fad4ca192849
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2025-01-01 17:10:08 +0000

    Diwali Discount problem from CodeChef

---
+++ CodeChef/Diwali_discount.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin >> a >> b;
+	if(b>=a){
+	    cout << 0 << endl;
+	}else{
+	    cout << a - b;
+	}
+}
