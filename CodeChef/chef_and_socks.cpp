f5e84f08b196dc53c1bd4ab5e6fc8b546f2afd4e
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2025-01-03 18:28:46 +0000

    Chef and Socks problem CodeChef

---
+++ CodeChef/chef_and_socks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,x,y;
+	cin >> a >> x >> y;
+	int total = y+x;
+	if(total>=a){
+	    cout << "YES" << endl;
+	}else{
+	    cout << "NO" << endl;
+	}
+}
