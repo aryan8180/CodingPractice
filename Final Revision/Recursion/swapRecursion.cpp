7f8f2a9eaaeb3d47ff6b9e5d394102d1ded31fb2
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2025-07-17 05:49:43 +0000

    Swapping Array, using recursion

---
+++ Final Revision/Recursion/swapRecursion.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[],int s,int e){
+    if(s>=e){
+        return;
+    }
+    swap(arr[s],arr[e]);
+    reverse(arr,s+1,e-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    reverse(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout << arr[i] << endl;
+    }
+}
